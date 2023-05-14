e1766199444534826e2f03593c8b8c64da1302c9
painnick <painnick@gmail.com>
2023-05-14 17:41:32 +0900

    UPDATE. ESP32CAM Pin 정보 설정

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,17 @@
 // Recommended pins include 2,4,12-19,21-23,25-27,32-33
 // for the ESP32-S2 the GPIO pins are 1-21,26,33-42
 
-// for ESP32 Microcontrollers
-#define PIN_DATA  21
-#define PIN_LATCH 22
-#define PIN_CLOCK 23
- 
+#define PIN_DATA  12
+#define PIN_LATCH 13
+#define PIN_CLOCK 15
+
+#define PIN_NOT_USED1 14
+#define PIN_NOT_USED2 2
+#define PIN_NOT_USED3 16
+
+#define PIN_FLASH_LED 4
+#define PIN_INTERNAL_LED 33 // INVERT
+
 byte shift_register = 0;
 
 void updateShiftRegister() {
@@ -25,7 +31,10 @@ void updateShiftRegister() {
 }
 
 void setup() {
-  pinMode(PIN_DATA, OUTPUT);  
+  pinMode(PIN_INTERNAL_LED, OUTPUT);
+
+  // For Shift-Register
+  pinMode(PIN_DATA, OUTPUT);
   pinMode(PIN_LATCH, OUTPUT);
   pinMode(PIN_CLOCK, OUTPUT);
 
