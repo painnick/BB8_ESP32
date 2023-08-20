0b49aad9f4384d2e46be4f420cce44ab59903fcc
painnick <painnick@gmail.com>
2023-08-20 22:17:33 +0900

    ADD. BT Debug

--- Body/src/main.cpp
+++ Body/src/main.cpp
@@ -2,6 +2,8 @@
 
 #include "esp_log.h"
 
+#include "BluetoothSerial.h"
+
 #define MAIN_TAG "Main"
 
 // These are all GPIO pins on the ESP32
@@ -24,189 +26,257 @@
 #include "MotorController.h"
 #include "Mp3Controller.h"
 #include "ShiftRegisterController.h"
-#include "SoftwareSerial.h"
 #include "VoiceRecognitionController.h"
 #include "LighterController.h"
 
+BluetoothSerial SerialBT;
+
 void randomMoveMotor(unsigned long duration, MotorCallback callback = nullptr,
                      unsigned long startDelayMs = 0) {
-  if ((random(1024) % 2) == 0)
-    motor1.left(duration, callback, startDelayMs);
-  else
-    motor1.right(duration, callback, startDelayMs);
+    if ((random(1024) % 2) == 0)
+        motor1.left(duration, callback, startDelayMs);
+    else
+        motor1.right(duration, callback, startDelayMs);
 }
 
 void moveMotorOpposite(MotorController *mc, MOTOR_DIRECTION dir,
                        unsigned long duration,
                        MotorCallback callback = nullptr) {
-  if (dir == MOTOR_DIRECTION::LEFT) {
-    mc->stop();
-    delay(100);
-    mc->right(duration, callback);
-  } else if (dir == MOTOR_DIRECTION::RIGHT) {
-    mc->stop();
-    delay(100);
-    mc->left(duration, callback);
-  }
+    if (dir == MOTOR_DIRECTION::LEFT) {
+        mc->stop();
+        delay(100);
+        mc->right(duration, callback);
+    } else if (dir == MOTOR_DIRECTION::RIGHT) {
+        mc->stop();
+        delay(100);
+        mc->left(duration, callback);
+    }
+}
+
+void commandHello() {
+    vr.loadWakeup();
+    monoEyeController.wakeUp();
+    randomPlayGeneral();
+    randomMoveMotor(
+            200 + random(0, 5) * 100,
+            [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
+                moveMotorOpposite(mc, dir, 200 + random(0, 5) * 100);
+            },
+            1000);
+    commander1.send("LED1ON");
+    commander1.send("LED2ON");
+    commander1.send("LED3ON");
+    commander1.send("WIFION");
+    shiftRegister.set(0xFF);
+}
+
+void commandBye() {
+    vr.loadDefault();
+    monoEyeController.sleep();
+    playBye();
+    motor1.stop();
+    commander1.send("LED1OFF");
+    commander1.send("LED2OFF");
+    commander1.send("LED3OFF");
+    commander1.send("WIFIOFF");
+    shiftRegister.set(0);
+}
+
+void commandStop() {
+    dfmp3.stop();
+    lighterController.off();
+    shiftRegister.randomLight(false);
+    motor1.stop();
+}
+
+void commandRight() {
+    motor1.right(500);
+}
+
+void commandLeft() {
+    motor1.left(500);
+}
+
+void commandFool() {
+    playFail();
+    shiftRegister.warningMessage();
+    randomMoveMotor(
+            500,
+            [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
+                moveMotorOpposite(
+                        mc, dir, 500,
+                        [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
+                            moveMotorOpposite(
+                                    mc, dir, 300,
+                                    [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
+                                        moveMotorOpposite(mc, dir, 300);
+                                    });
+                        });
+            },
+            500);
+}
+
+void commandMusic() {
+    motor1.stop();
+    shiftRegister.randomLight(true);
+    playOST();
+}
+
+void commandLight() {
+    lighterController.on();
 }
 
 int lastCommand = -1;
 int duplicateCommandCount = 0;
+
 void setup() {
 #ifdef DEBUG
-  ESP_LOGI(MAIN_TAG, "Setup...");
+    ESP_LOGI(MAIN_TAG, "Setup...");
 #endif
 
-  setupSound();
+    SerialBT.begin("BB-8");
 
-  vr.init([](int cmd) -> void {
-    if (cmd < 0) {
-      return;
-    }
+    setupSound();
 
-    if (lastCommand == cmd) {
-      ++duplicateCommandCount;
-    } else {
-      duplicateCommandCount = 1;
-    }
-    lastCommand = cmd;
+    vr.init([](int cmd) -> void {
+        if (cmd < 0) {
+            return;
+        }
 
-    if (duplicateCommandCount >= COMMAND_REPETITION_COUNT) {
+        if (lastCommand == cmd) {
+            ++duplicateCommandCount;
+        } else {
+            duplicateCommandCount = 1;
+        }
+        lastCommand = cmd;
+
+        if (duplicateCommandCount >= COMMAND_REPETITION_COUNT) {
 #ifdef DEBUG
-      ESP_LOGD(MAIN_TAG, "Same command %dtimes", duplicateCommandCount);
+            ESP_LOGD(MAIN_TAG, "Same command %dtimes", duplicateCommandCount);
 #endif
-      playWhy();
-      randomMoveMotor(
-          1500,
-          [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
-            moveMotorOpposite(mc, dir, 500);
-          },
-          1000);
-      shiftRegister.warningMessage();
-      duplicateCommandCount = 0;
-      return;
-    }
+            playWhy();
+            randomMoveMotor(
+                    1500,
+                    [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
+                        moveMotorOpposite(mc, dir, 500);
+                    },
+                    1000);
+            shiftRegister.warningMessage();
+            duplicateCommandCount = 0;
+            return;
+        }
 
-    if (random(32) == 3) {
-      playFail();
-      randomMoveMotor(600, nullptr, 1000);
-      return;
-    }
+        if (random(32) == 3) {
+            playFail();
+            randomMoveMotor(600, nullptr, 1000);
+            return;
+        }
 
-    switch (vr.convertToSig(cmd)) {
-    case VR_HELLO: // HELLO
-      vr.loadWakeup();
-      monoEyeController.wakeUp();
-      randomPlayGeneral();
-      randomMoveMotor(
-          200 + random(0, 5) * 100,
-          [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
-            moveMotorOpposite(mc, dir, 200 + random(0, 5) * 100);
-          },
-          1000);
-      commander1.send("LED1ON");
-      commander1.send("LED2ON");
-      commander1.send("LED3ON");
-      commander1.send("WIFION");
-      shiftRegister.set(0xFF);
-      break;
-    case VR_BYE: // BYE
-      vr.loadDefault();
-      monoEyeController.sleep();
-      playBye();
-      motor1.stop();
-      commander1.send("LED1OFF");
-      commander1.send("LED2OFF");
-      commander1.send("LED3OFF");
-      commander1.send("WIFIOFF");
-      shiftRegister.set(0);
-      break;
-    case VR_RIGHT: // RIGHT
-      motor1.right(500);
-      break;
-    case VR_LEFT: // LEFT
-      motor1.left(500);
-      break;
-    case VR_STOP: // STOP
-      dfmp3.stop();
-      lighterController.off();
-      shiftRegister.randomLight(false);
-      motor1.stop();
-      break;
-    case VR_FOOL: // FOOL!
-      playFail();
-      shiftRegister.warningMessage();
-      randomMoveMotor(
-          500,
-          [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
-            moveMotorOpposite(
-                mc, dir, 500,
-                [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
-                  moveMotorOpposite(
-                      mc, dir, 300,
-                      [](MotorController *mc, MOTOR_DIRECTION dir) -> void {
-                        moveMotorOpposite(mc, dir, 300);
-                      });
-                });
-          },
-          500);
-      break;
-    case VR_MUSIC: // MUSIC
-      motor1.stop();
-      shiftRegister.randomLight(true);
-      playOST();
-      break;
-    case VR_LIGHT:
-      lighterController.on();
-      break;
-    default:
-      dfmp3.stop();
-      break;
-    }
-  });
+        switch (vr.convertToSig(cmd)) {
+            case VR_HELLO: // HELLO
+                commandHello();
+                break;
+            case VR_BYE: // BYE
+                commandBye();
+                break;
+            case VR_RIGHT: // RIGHT
+                commandRight();
+                break;
+            case VR_LEFT: // LEFT
+                commandLeft();
+                break;
+            case VR_STOP: // STOP
+                commandStop();
+                break;
+            case VR_FOOL: // FOOL!
+                commandFool();
+                break;
+            case VR_MUSIC: // MUSIC
+                commandMusic();
+                break;
+            case VR_LIGHT:
+                commandLight();
+                break;
+            default:
+                dfmp3.stop();
+                break;
+        }
+    });
 
-  motor1.init();
+    motor1.init();
 
-  commander1.init([](const Commander *, const String &cmd) -> void {
+    commander1.init([](const Commander *, const String &cmd) -> void {
 #ifdef DEBUG
-    ESP_LOGD(MAIN_TAG, "<= Recv : %s", cmd.c_str());
+        ESP_LOGD(MAIN_TAG, "<= Recv : %s", cmd.c_str());
 #endif
-    if (cmd == "Left") {
-      motor1.left(200);
-    } else if (cmd == "Right") {
-      motor1.right(200);
-    }
-  });
+        if (cmd == "Left") {
+            motor1.left(200);
+        } else if (cmd == "Right") {
+            motor1.right(200);
+        }
+    });
 
-  randomPlayGeneral();
+    randomPlayGeneral();
 
 #ifdef DEBUG
-  // Head의 기존 상태 값을 초기화
-  commander1.send("LED1OFF");
-  commander1.send("LED2OFF");
-  commander1.send("LED3OFF");
-  commander1.send("WIFIOFF");
+    // Head의 기존 상태 값을 초기화
+    commander1.send("LED1OFF");
+    commander1.send("LED2OFF");
+    commander1.send("LED3OFF");
+    commander1.send("WIFIOFF");
 #endif
 
-  shiftRegister.warningMessage();
+    shiftRegister.warningMessage();
 
 #ifdef DEBUG
-  ESP_LOGI(MAIN_TAG, "Setup Body");
+    ESP_LOGI(MAIN_TAG, "Setup Body");
 #endif
 }
 
 unsigned long lastSaid = 0;
+
 void loop() {
-  unsigned long now = millis();
-  monoEyeController.loop(now);
-  if (now - lastSaid > 1000 * SAY_ANYTHING_INTERVAL_SEC) {
-    randomPlayGeneral();
-    lastSaid = now;
-  }
-  motor1.loop(now);
-  commander1.loop();
-  shiftRegister.loop(now);
-  lighterController.loop(now);
-  vr.loop();
-  dfmp3.loop();
+    if (SerialBT.available()) {
+        String btCmd = SerialBT.readString();
+        ESP_LOGD(MAIN_TAG, "BT >> %s", btCmd.c_str());
+        if (btCmd == "hello") {
+            commandHello();
+        } else if (btCmd == "bye") {
+            commandBye();
+        } else if (btCmd == "right") {
+            commandRight();
+        } else if (btCmd == "left") {
+            commandLeft();
+        } else if (btCmd == "stop") {
+            commandStop();
+        } else if (btCmd == "fool") {
+            commandFool();
+        } else if (btCmd == "music") {
+            commandMusic();
+        } else if (btCmd == "light") {
+            commandLight();
+        } else if (btCmd == "help") {
+            SerialBT.println("hello");
+            SerialBT.println("bye");
+            SerialBT.println("right");
+            SerialBT.println("left");
+            SerialBT.println("stop");
+            SerialBT.println("fool");
+            SerialBT.println("music");
+            SerialBT.println("light");
+        }
+    }
+
+    unsigned long now = millis();
+    monoEyeController.loop(now);
+    if (now - lastSaid > 1000 * SAY_ANYTHING_INTERVAL_SEC) {
+        randomPlayGeneral();
+        lastSaid = now;
+    }
+    motor1.loop(now);
+    commander1.loop();
+    shiftRegister.loop(now);
+    lighterController.loop(now);
+    vr.loop();
+    dfmp3.loop();
 }
