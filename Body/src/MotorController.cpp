8bb475f67e28e391a66667910d14098f65dfc356
painnick <painnick@gmail.com>
2023-08-21 17:05:31 +0900

    REFACTORING. MotorController

--- Body/src/MotorController.cpp
+++ Body/src/MotorController.cpp
@@ -1,91 +1,92 @@
 #include "MotorController.h"
 
-MotorController::MotorController() : endMoveMs(0), dir(MOTOR_DIRECTION::STOP) {}
+#include <utility>
 
-MotorController::~MotorController() {}
+MotorController::MotorController() : startMoveMs(0), endMoveMs(0), dir(MOTOR_DIRECTION::STOP) {}
+
+MotorController::~MotorController() = default;
 
 void MotorController::init() {
-  pinMode(PIN_MOTOR1, OUTPUT);
-  pinMode(PIN_MOTOR2, OUTPUT);
+    pinMode(PIN_MOTOR1, OUTPUT);
+    pinMode(PIN_MOTOR2, OUTPUT);
 }
 
 void MotorController::left(unsigned long ms, MotorCallback cb,
                            unsigned long startDelayMs) {
-  callback = cb;
-  unsigned long now = millis();
-  endMoveMs = now + ms + startDelayMs;
-  if (startDelayMs == 0) {
-    startMoveMs = 0;
-    internalLeft();
-  } else {
-    dir = MOTOR_DIRECTION::LEFT;
-    startMoveMs = now + startDelayMs;
-  }
+    callback = std::move(cb);
+    unsigned long now = millis();
+    endMoveMs = now + ms + startDelayMs;
+    if (startDelayMs == 0) {
+        startMoveMs = 0;
+        internalLeft();
+    } else {
+        dir = MOTOR_DIRECTION::LEFT;
+        startMoveMs = now + startDelayMs;
+    }
 }
 
 void MotorController::right(unsigned long ms, MotorCallback cb,
                             unsigned long startDelayMs) {
-  callback = cb;
-  unsigned long now = millis();
-  endMoveMs = now + ms + startDelayMs;
-  if (startDelayMs == 0) {
-    startMoveMs = 0;
-    internalRight();
-  } else {
-    dir = MOTOR_DIRECTION::RIGHT;
-    startMoveMs = now + startDelayMs;
-  }
+    callback = cb;
+    unsigned long now = millis();
+    endMoveMs = now + ms + startDelayMs;
+    if (startDelayMs == 0) {
+        startMoveMs = 0;
+        internalRight();
+    } else {
+        dir = MOTOR_DIRECTION::RIGHT;
+        startMoveMs = now + startDelayMs;
+    }
 }
 
 void MotorController::loop(unsigned long now) {
-  if (startMoveMs != 0) {
-    if (now > startMoveMs) {
-      startMoveMs = 0;
-      if (dir == MOTOR_DIRECTION::LEFT) {
-        internalLeft();
-      } else if (dir == MOTOR_DIRECTION::RIGHT) {
-        internalRight();
-      }
+    if (startMoveMs != 0) {
+        if (now > startMoveMs) {
+            startMoveMs = 0;
+            if (dir == MOTOR_DIRECTION::LEFT) {
+                internalLeft();
+            } else if (dir == MOTOR_DIRECTION::RIGHT) {
+                internalRight();
+            }
+        }
     }
-  }
 
-  if (dir != MOTOR_DIRECTION::STOP) {
-    unsigned long now = millis();
-    if (endMoveMs - now < 100) {
-      MotorCallback lastCallback = callback;
-      MOTOR_DIRECTION lastDir = dir;
-      stop();
-      if (lastCallback != nullptr) {
-        lastCallback(this, lastDir);
-      }
+    if (dir != MOTOR_DIRECTION::STOP) {
+        if (endMoveMs - now < 100) {
+            MotorCallback lastCallback = callback;
+            MOTOR_DIRECTION lastDir = dir;
+            stop();
+            if (lastCallback != nullptr) {
+                lastCallback(this, lastDir);
+            }
+        }
     }
-  }
 }
 
 void MotorController::internalLeft() {
-  dir = MOTOR_DIRECTION::LEFT;
-  digitalWrite(PIN_MOTOR1, HIGH);
-  digitalWrite(PIN_MOTOR2, LOW);
+    dir = MOTOR_DIRECTION::LEFT;
+    digitalWrite(PIN_MOTOR1, HIGH);
+    digitalWrite(PIN_MOTOR2, LOW);
 #ifdef DEBUG
-  ESP_LOGD(MOTOR_TAG, "Left");
+    ESP_LOGD(MOTOR_TAG, "Left");
 #endif
 }
 
 void MotorController::internalRight() {
-  dir = MOTOR_DIRECTION::RIGHT;
-  digitalWrite(PIN_MOTOR1, LOW);
-  digitalWrite(PIN_MOTOR2, HIGH);
+    dir = MOTOR_DIRECTION::RIGHT;
+    digitalWrite(PIN_MOTOR1, LOW);
+    digitalWrite(PIN_MOTOR2, HIGH);
 #ifdef DEBUG
-  ESP_LOGD(MOTOR_TAG, "Right");
+    ESP_LOGD(MOTOR_TAG, "Right");
 #endif
 }
 
 void MotorController::stop() {
-  dir = MOTOR_DIRECTION::STOP;
-  digitalWrite(PIN_MOTOR1, LOW);
-  digitalWrite(PIN_MOTOR2, LOW);
+    dir = MOTOR_DIRECTION::STOP;
+    digitalWrite(PIN_MOTOR1, LOW);
+    digitalWrite(PIN_MOTOR2, LOW);
 #ifdef DEBUG
-  ESP_LOGD(MOTOR_TAG, "Stop");
+    ESP_LOGD(MOTOR_TAG, "Stop");
 #endif
 }
 
--- Body/src/MotorController.h
+++ Body/src/MotorController.h
@@ -11,34 +11,42 @@
 #define PIN_MOTOR2 19
 
 enum class MOTOR_DIRECTION {
-  STOP = 0,
-  LEFT = 1,
-  RIGHT = 2,
+    STOP = 0,
+    LEFT = 1,
+    RIGHT = 2,
 };
 
 class MotorController;
+
 typedef std::function<void(MotorController *, MOTOR_DIRECTION)> MotorCallback;
 
 class MotorController {
 public:
-  MotorController();
-  ~MotorController();
-  void init();
-  void left(unsigned long ms, MotorCallback callback = nullptr,
-            unsigned long startDelatMs = 0);
-  void right(unsigned long ms, MotorCallback callback = nullptr,
-             unsigned long startDelatMs = 0);
-  void stop();
-  void loop(unsigned long now);
+    MotorController();
+
+    ~MotorController();
+
+    void init();
+
+    void left(unsigned long ms, MotorCallback callback = nullptr,
+              unsigned long startDeltaMs = 0);
+
+    void right(unsigned long ms, MotorCallback callback = nullptr,
+               unsigned long startDeltaMs = 0);
+
+    void stop();
+
+    void loop(unsigned long now);
 
 private:
-  unsigned long startMoveMs;
-  unsigned long endMoveMs;
-  MOTOR_DIRECTION dir;
-  MotorCallback callback;
+    unsigned long startMoveMs;
+    unsigned long endMoveMs;
+    MOTOR_DIRECTION dir;
+    MotorCallback callback;
 
-  void internalLeft();
-  void internalRight();
+    void internalLeft();
+
+    void internalRight();
 };
 
 extern MotorController motor1;
