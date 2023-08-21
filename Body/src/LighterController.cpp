117356c7812eb0e257001f45465544b5ea6ab712
painnick <painnick@gmail.com>
2023-08-21 17:01:52 +0900

    REFACTORING. LighterController

--- Body/src/LighterController.cpp
+++ Body/src/LighterController.cpp
@@ -7,29 +7,28 @@
 #define PWM_FREQUENCY 1000
 #define PWM_RESOLUTION 8
 
-LighterController::LighterController(uint8_t pinNo)
-    : pinNo(pinNo) {
-  ledcSetup(ledChannel, PWM_FREQUENCY, PWM_RESOLUTION);
-  ledcAttachPin(pinNo, ledChannel);
+LighterController::LighterController(uint8_t pinNo) {
+    ledcSetup(ledChannel, PWM_FREQUENCY, PWM_RESOLUTION);
+    ledcAttachPin(pinNo, ledChannel);
 }
 
 void LighterController::on() {
-  isOn = true;
+    isOn = true;
 }
 
 void LighterController::off() {
-  isOn = false;
-  ledcWrite(ledChannel, 0);
+    isOn = false;
+    ledcWrite(ledChannel, 0);
 }
 
 void LighterController::loop(unsigned long now) {
-  if (isOn) {
-    if (now - lastChecked > CHECK_INTERVAL_MS) {
-      int bright = random(127, 255);
-      ledcWrite(ledChannel, bright);
-      lastChecked = now;
+    if (isOn) {
+        if (now - lastChecked > CHECK_INTERVAL_MS) {
+            int bright = random(127, 255);
+            ledcWrite(ledChannel, bright);
+            lastChecked = now;
+        }
     }
-  }
 }
 
 LighterController lighterController(PIN_LIGHTER);
--- Body/src/LighterController.h
+++ Body/src/LighterController.h
@@ -6,17 +6,19 @@
 
 class LighterController {
 public:
-  LighterController(uint8_t pinNo);
-  void on();
-  void off();
-  void loop(unsigned long now);
+    explicit LighterController(uint8_t pinNo);
+
+    void on();
+
+    void off();
+
+    void loop(unsigned long now);
 
 private:
-  uint8_t pinNo;
-  const uint8_t ledChannel{7};
-  boolean isOn{false};
-  unsigned int tick{};
-  unsigned int lastChecked{};
+    const uint8_t ledChannel{7};
+    boolean isOn{false};
+    boolean isChanged{false};
+    unsigned int lastChecked{};
 };
 
 extern LighterController lighterController;
